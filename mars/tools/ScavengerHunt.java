436923e634783ee66b9283b253aeb406fedbdf32
Brankale <45881310+Brankale@users.noreply.github.com>
2020-09-26 11:15:07 +0200

    removed unnecessary imports

--- mars/tools/ScavengerHunt.java
+++ mars/tools/ScavengerHunt.java
@@ -8,8 +8,6 @@ import mars.util.Binary;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Observable;
